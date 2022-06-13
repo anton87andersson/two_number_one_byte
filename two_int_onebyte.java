0db1a315deb9691925b549eb14b2b0d0fba2d8fa
Anton Andersson <62707484+anton87andersson@users.noreply.github.com>
2022-06-13 18:20:57 +0200

    Create two_int_onebyte.java

---
+++ two_int_onebyte.java
@@ -0,0 +1,10 @@
+public class two_int_onebyte {
+    public static void main(String[] args) { 
+        byte c;    
+        c=(byte)(9<<4);
+        c=(byte)(c|12);
+
+        System.out.println((c&0b11110000)>>4);
+        System.out.println((c&0b00001111));
+    }
+}
